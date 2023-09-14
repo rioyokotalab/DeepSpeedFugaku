1e85a308b074bc908cf6b010b2355eaaaa8dccfb
idaten459 <shukai2013@gmail.com>
2023-09-15 08:58:00 +0900

    argument pass None

--- pretrain_gpt.py
+++ pretrain_gpt.py
@@ -346,7 +346,7 @@ if __name__ == "__main__":
     git_ds_info()
     from torch.profiler import profile, ProfilerActivity
     with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
-        pretrain(train_valid_test_datasets_provider, model_provider, forward_step,
+        pretrain(None, None, None,
                  args_defaults={'tokenizer_type': 'GPT2BPETokenizer'},
-                 data_post_process=data_post_process)
+                 data_post_process=None)
     print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=100))
