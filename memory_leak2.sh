20f541d35e3fa2bf3d7972e41eb4de86a0a71c4c
idaten459 <shukai2013@gmail.com>
2023-09-15 09:50:08 +0900

    add comment

--- memory_leak2.sh
+++ memory_leak2.sh
@@ -9,47 +9,18 @@
 #PJM -j
 #PJM -S
 
-# # if fugaku environment
-# if [ "$USER" == "u01959" ]; then
-
-# # notice start job
-# export PATH=$PATH:/data/hp190122/users/u01959/jobscheduler2slack
-# post_message start_job
-
-# # activate pytorch environment
-# source /data/hp190122/share/PyTorch-1.10.1/env.src
-# export PYTHONUSERBASE=$HOME/work/.local
-# export PATH=$PATH:$PYTHONUSERBASE/bin
-# fi
-# # setting distribute
-# MASTER_ADDR=localhost
-# MASTER_PORT=$((10000 + ($PJM_JOBID % 50000)))
-# WORLD_SIZE=2
-# output_path="jobs/torch.1.10/outs/${PJM_JOBID}_n${nodos}"
-
-# # run
-# if [ "$USER" == "u01959" ]; then
-# MASTER_ADDR=$MASTER_ADDR \
-# MASTER_PORT=$MASTER_PORT \
-# mpiexec -n ${WORLD_SIZE} -std-proc ${output_path}/stdproc \
-# -x LD_PRELOAD="/vol0003/hp190122/data/users/u01959/my_mpi_allreduce/only_override.so" \
-# python memory_leak.py
-# else
-# MASTER_ADDR=$MASTER_ADDR \
-# MASTER_PORT=$MASTER_PORT \
-# mpiexec -n ${WORLD_SIZE} \
-# python memory_leak2.py
-# fi
-
-export PATH=$PATH:/data/hp190122/users/u01959/jobscheduler2slack
-post_message start_job
+# load pytorch
 source /data/hp190122/share/PyTorch-1.10.1/env.src
 export PYTHONUSERBASE=$HOME/work/.local
 export PATH=$PATH:$PYTHONUSERBASE/bin
+
+# set up environment
 MASTER_ADDR=localhost
 MASTER_PORT=$((10000 + ($PJM_JOBID % 50000)))
 WORLD_SIZE=2
 output_path="jobs/torch.1.10/outs/${PJM_JOBID}_n${nodos}"
+
+# run
 MASTER_ADDR=$MASTER_ADDR \
 MASTER_PORT=$MASTER_PORT \
 mpiexec -n ${WORLD_SIZE} -std-proc ${output_path}/stdproc \
