f7859b343cdc6fcafd8b1ba56f1a1bd87f062d65
kazuki <kaz.tokyo.tech20@gmail.com>
2023-09-21 11:39:44 +0900

    chore: wandb setting update

--- megatron/global_vars.py
+++ megatron/global_vars.py
@@ -173,7 +173,7 @@ def _set_wandb_writer(args):
                 "entity": entity,
                 "name": exp_name,
                 "config": args,
-                "project": "megatron-deepspeed-loss-spike",
+                "project": "llm-jp-v1-dataset",
             }
             if args.wandb_id is not None:
                 wandb_input["id"] = args.wandb_id
