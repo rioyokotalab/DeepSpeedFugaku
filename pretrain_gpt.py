79973c4c206bcb33dc3f2f1d247b477851e126be
idaten459 <shukai2013@gmail.com>
2023-09-25 12:17:10 +0900

    profileを戻すとリーク

--- pretrain_gpt.py
+++ pretrain_gpt.py
@@ -1,6 +1,8 @@
 from megatron.training import pretrain
 
 if __name__ == "__main__":
-    pretrain(None, None, None,
-                args_defaults={'tokenizer_type': 'GPT2BPETokenizer'},
-                data_post_process=None)
+    from torch.profiler import profile, ProfilerActivity
+    with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+        pretrain(None, None, None,
+                    args_defaults={'tokenizer_type': 'GPT2BPETokenizer'},
+                    data_post_process=None)
