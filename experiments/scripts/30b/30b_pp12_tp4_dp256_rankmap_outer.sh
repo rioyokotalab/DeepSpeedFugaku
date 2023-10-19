bc1a46c3151a87d6cf5b34a14c526f09bdee3a31
u11890 <takumioka428@gmail.com>
2023-10-19 11:04:31 +0900

    add scripts

---
+++ experiments/scripts/30b/30b_pp12_tp4_dp256_rankmap_outer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#PJM -L "rscunit=rscunit_ft01,rscgrp=ppu2023"
+#PJM -L elapse=10:00:00
+#PJM -L "node=8x48x32:torus:strict-io"
+#PJM -L "freq=2200"
+#PJM -L "throttling_state=0"
+#PJM -L "issue_state=0"
+#PJM -L "ex_pipe_state=0"
+#PJM -L "eco_state=0"
+#PJM -L "retention_state=0"
+#PJM --llio localtmp-size=80Gi
+#PJM --mpi "proc=1"
+#PJM --mpi "max-proc-per-node=1"
+#PJM -g hp230254
+#PJM -x PJM_LLIO_GFSCACHE=/vol0003:/vol0004:/vol0005:/vol0006
+#PJM -j
+#PJM -S
+
+hostfile_name='4x16x16x2x3x2_tp4dp256pp12'
+stdproc_name='jobs/30b_pp12_tp4_dp256_pytorch1.13/outs/${PJM_JOBID}_n/stdproc'
+inner_file_name='30b_pp12_tp4_dp256_rankmap_inner.sh'
+num_node=12288
+
+rm /home/u11890/work/rankmap/vcoordfile_${hostfile_name}
+
+mpirun -n ${num_node} /home/u11890/work/rankmap/fjmpi_6d_to_3d.out /home/u11890/work/rankmap/hostfile_${hostfile_name} /home/u11890/work/rankmap/vcoordfile_${hostfile_name}
+
+mpirun -n ${num_node} \
+	--vcoordfile /home/u11890/work/rankmap/vcoordfile_${hostfile_name} \
+	-std-proc ${stdproc_name} \
+	bash ${inner_file_name}
---
+++ experiments/scripts/30b/30b_pp8_tp6_dp256_rankmap_inner.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+param_name='30b_pp8_tp6_dp256_pytorch1.13'
+NNODES=12288
+DATA_PARALLEL_SIZE=256
+PIPELINE_MODEL_PARALLEL_SIZE=8
+TENSOR_MODEL_PARALLEL_SIZE=6
+
+mkdir -p /local/fcc/pytorch
+cd /local/fcc
+tar xf /home/u11890/work/1693389241.318550480.fcc.pytorch.y.r1.13_for_a64fx.tar
+source /local/fcc/inst/venv/bin/activate
+cd /home/u11890/work/timer/DeepSpeedFugaku
+
+# Change for multinode config
+CPUS_PER_NODE=1
+NODE_RANK=0
+export WORLD_SIZE=$(($CPUS_PER_NODE*$NNODES))
+export MASTER_ADDR=localhost
+export MASTER_PORT=$((10000 + ($PJM_JOBID % 50000)))
+export TIMER="timer.${param_name}"
+CHECKPOINT_PATH=checkpoints/${param_name}/
+INPUT_PREFIX=dataset
+VOCAB_FILE=tokenizer/models/ver2/code20k_en40k_ja60k.ver2.1.model
+DATA_PATH=dataset/codeparrot_content_document
+TENSORBOARD_ARGS="--tensorboard-dir experiments/tensorboard"
+
+PIPELINE_PARALLEL_ARGS="--pipeline-model-parallel-size $PIPELINE_MODEL_PARALLEL_SIZE"
+MODEL_PARALLEL_ARGS="--tensor-model-parallel-size $TENSOR_MODEL_PARALLEL_SIZE"
+PARALLEL_ARGS="$MODEL_PARALLEL_ARGS $PIPELINE_PARALLEL_ARGS"
+
+export OMP_NUM_THREADS=48
+export LD_PRELOAD=/local/fcc/inst/other/lib/libtcmalloc.so
+#export OMP_WAIT_POLICY=ACTIVE
+
+numactl -m 4-7 -N 4-7 python pretrain_gpt.py \
+    --num-layers 48 \
+    --hidden-size 6912 \
+    --num-attention-heads 72 \
+    --micro-batch-size 1 \
+    --global-batch-size 1536 \
+    --seq-length 2048 \
+    --max-position-embeddings 2048 \
+    --train-iters 5 \
+    --lr-decay-iters 320000 \
+    --save $CHECKPOINT_PATH \
+    --data-path $DATA_PATH \
+    --tokenizer-type JapaneseSentencePiece \
+    --vocab-file $VOCAB_FILE \
+    --data-impl mmap \
+    --split 949,50,1 \
+    --distributed-backend mpi \
+    --init-method-std 0.013 \
+    --lr 0.00015 \
+    --min-lr 1.0e-5 \
+    --lr-decay-style cosine \
+    --weight-decay 1e-2 \
+    --cpu-optimizer \
+    --cpu-torch-adam \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.95 \
+    --clip-grad 1.0 \
+    --lr-warmup-fraction .01 \
+    --checkpoint-activations \
+    --log-interval 1 \
+    --save-interval 10000 \
+    --eval-interval 1000 \
+    --eval-iters 5 \
+    --no-cuda \
+    --use-cpu-initialization \
+    --num-workers 0 \
+    --no-load-rng \
+    $PARALLEL_ARGS \
+    --use-timer \
+    --use-flush-denormal
