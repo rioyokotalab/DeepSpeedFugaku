41acecccf11f974119b86114809c0d69b0b209fe
Akihiro Tabuchi <tabuchi.akihiro@fujitsu.com>
2024-02-02 11:43:22 +0900

    Fix scripts for after 400b

--- scripts/13b/13b_pp8_tp6_dp288_after400b_inner.sh
+++ scripts/13b/13b_pp8_tp6_dp288_after400b_inner.sh
@@ -45,8 +45,8 @@ mkdir -p $CHECKPOINT_PATH
 
 # dataset setting
 DATASET_PATH_LIST=(
-    /2ndfs/hp230254/u11887/dataset/llm-jp-corpus-v1.0.2/fugaku_13b/binarized/v2_2-code10k_en20k_ja30k
-    /vol0503/data/hp230254/dataset/llm-jp-corpus-v1.0.2/fugaku_13b/binarized/v2_2-code10k_en20k_ja30k
+#    /2ndfs/hp230254/u11887/dataset/llm-jp-corpus-v1.0.2/fugaku_13b/binarized/v2_2-code10k_en20k_ja30k
+    /vol0503/data/hp230254/dataset/llm-jp-corpus-v1.0.2/fugaku_13b/binarized/v2_2-code10k_en20k_ja30k_rest80b
 )
 
 # read from multiple volumes
@@ -61,78 +61,78 @@ DATASET_PATH=${DATASET_PATH_LIST[$(( PMIX_RANK % ${#DATASET_PATH_LIST[*]} ))]}
 # train data setting
 TRAIN_DATA_PATH=""
 
-# en books
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 21533774460 ${DATASET_PATH}/books_merged_text_document"
-# en arxiv
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14300397157 ${DATASET_PATH}/lumi_en_arxiv_merge_text_document"
-# en falcon (refined-web)
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 11253894954 ${DATASET_PATH}/lumi_en_falcon_merge_text_document"
-# en pile free-law
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 11955669467 ${DATASET_PATH}/pile_FreeLaw_merge_text_document"
-# en pile pubmed
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 5308757461 ${DATASET_PATH}/pile_pubmed_merge_text_document"
-# en red-pajama cc
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 31674422340 ${DATASET_PATH}/red_pajama_cc_merge_text_document"
-# en red-pajama arxiv
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 9562707240 ${DATASET_PATH}/red_pajama_arxiv_merge_text_document"
-# en 10_k
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 369623371 ${DATASET_PATH}/10_k_text_document"
-# en atticus is contracts and legal
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 10696864 ${DATASET_PATH}/atticus_cuad_muad_contracts_text_document"
-# en pile philarchive
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 347850794 ${DATASET_PATH}/pile_PhilArchive_text_document"
-# en pile nih text
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 422999727 ${DATASET_PATH}/pile_NIH_text_document"
-# en parliament
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 133677757 ${DATASET_PATH}/parliament_text_document"
-# en climabench
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 9218044 ${DATASET_PATH}/climabench_text_document"
-# en redpajama-stackexchange
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 4602769439 ${DATASET_PATH}/red_pajama_stackexchange_text_document"
-# en pile stackexchange
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 2588691546 ${DATASET_PATH}/pile_stackexchange_text_document"
-# en pile uspto
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 6484246795 ${DATASET_PATH}/pile_uspto_merge_text_document"
-# en wiki
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 5084807913 ${DATASET_PATH}/en_wiki_merged_text_document"
-
-# ja wiki
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 1593695182 ${DATASET_PATH}/ja_wiki_merged_text_document"
+## en books
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 21533774460 ${DATASET_PATH}/books_merged_text_document"
+## en arxiv
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14300397157 ${DATASET_PATH}/lumi_en_arxiv_merge_text_document"
+## en falcon (refined-web)
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 11253894954 ${DATASET_PATH}/lumi_en_falcon_merge_text_document"
+## en pile free-law
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 11955669467 ${DATASET_PATH}/pile_FreeLaw_merge_text_document"
+## en pile pubmed
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 5308757461 ${DATASET_PATH}/pile_pubmed_merge_text_document"
+## en red-pajama cc
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 31674422340 ${DATASET_PATH}/red_pajama_cc_merge_text_document"
+## en red-pajama arxiv
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 9562707240 ${DATASET_PATH}/red_pajama_arxiv_merge_text_document"
+## en 10_k
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 369623371 ${DATASET_PATH}/10_k_text_document"
+## en atticus is contracts and legal
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 10696864 ${DATASET_PATH}/atticus_cuad_muad_contracts_text_document"
+## en pile philarchive
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 347850794 ${DATASET_PATH}/pile_PhilArchive_text_document"
+## en pile nih text
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 422999727 ${DATASET_PATH}/pile_NIH_text_document"
+## en parliament
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 133677757 ${DATASET_PATH}/parliament_text_document"
+## en climabench
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 9218044 ${DATASET_PATH}/climabench_text_document"
+## en redpajama-stackexchange
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 4602769439 ${DATASET_PATH}/red_pajama_stackexchange_text_document"
+## en pile stackexchange
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 2588691546 ${DATASET_PATH}/pile_stackexchange_text_document"
+## en pile uspto
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 6484246795 ${DATASET_PATH}/pile_uspto_merge_text_document"
+## en wiki
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 5084807913 ${DATASET_PATH}/en_wiki_merged_text_document"
+#
+## ja wiki
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 1593695182 ${DATASET_PATH}/ja_wiki_merged_text_document"
 # cyberagent filtered ja
 TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 31808623323 ${DATASET_PATH}/ca_filter2ca_cc_filtered_org-bwords_reform_text_document"
 TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 22976439227 ${DATASET_PATH}/ca_filter2ca_cc2_filtered_org-bwords_text_document"
-# okazaki lab cc ja
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14184147835 ${DATASET_PATH}/split_reformat_0_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14440507382 ${DATASET_PATH}/split_reformat_1_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 13821488987 ${DATASET_PATH}/split_reformat_2_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 16717225516 ${DATASET_PATH}/split_reformat_3_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 16634821784 ${DATASET_PATH}/split_reformat_4_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 22117133216 ${DATASET_PATH}/split_reformat_5_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 25942389333 ${DATASET_PATH}/split_reformat_6_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 37326773828 ${DATASET_PATH}/split_reformat_7_text_document"
-
-# math llemma
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 11068009210 ${DATASET_PATH}/EleutherAI___proof-pile-2_algebraic-stack_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14680659112 ${DATASET_PATH}/EleutherAI___proof-pile-2_open-web-math_text_document"
-
-# code
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 16945303758 ${DATASET_PATH}/markdown_part_merge_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 2159043136 ${DATASET_PATH}/rust_part_merge_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 973518264 ${DATASET_PATH}/tex_part_merge_text_document"
-TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 10966015578 ${DATASET_PATH}/python_part_merge_text_document"
+## okazaki lab cc ja
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14184147835 ${DATASET_PATH}/split_reformat_0_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14440507382 ${DATASET_PATH}/split_reformat_1_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 13821488987 ${DATASET_PATH}/split_reformat_2_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 16717225516 ${DATASET_PATH}/split_reformat_3_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 16634821784 ${DATASET_PATH}/split_reformat_4_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 22117133216 ${DATASET_PATH}/split_reformat_5_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 25942389333 ${DATASET_PATH}/split_reformat_6_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 37326773828 ${DATASET_PATH}/split_reformat_7_text_document"
+#
+## math llemma
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 11068009210 ${DATASET_PATH}/EleutherAI___proof-pile-2_algebraic-stack_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 14680659112 ${DATASET_PATH}/EleutherAI___proof-pile-2_open-web-math_text_document"
+#
+## code
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 16945303758 ${DATASET_PATH}/markdown_part_merge_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 2159043136 ${DATASET_PATH}/rust_part_merge_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 973518264 ${DATASET_PATH}/tex_part_merge_text_document"
+#TRAIN_DATA_PATH="${TRAIN_DATA_PATH} 10966015578 ${DATASET_PATH}/python_part_merge_text_document"
 
 # training setting
-train_token_in_billion=400
+train_token_in_billion=80
 train_token=$(echo "$train_token_in_billion * 1000 * 1000 * 1000" | bc)
 train_token=$(echo "$train_token/1" | bc)
 
 # default megatron-deepspeed confgiraution is 3000 million, but they train model using 300 billion tokens.
-lr_warmup_tokens_in_billion=4
+lr_warmup_tokens_in_billion=0
 lr_warmup_tokens=$(echo "$lr_warmup_tokens_in_billion * 1000 * 1000 * 1000" | bc)
 lr_warmup_tokens=$(echo "$lr_warmup_tokens/1" | bc)
 
 # same as megatron deepspeed setting
-lr_decay_tokens_in_billion=${train_token_in_billion}
+lr_decay_tokens_in_billion=0 #${train_token_in_billion}
 lr_decay_tokens=${train_token}
 
 # logging setting
@@ -168,14 +168,24 @@ else
 fi
 echo ${NUM_WORKERS}
 
-USE_CACHED_DATASET="--use-cached-dataset"
+#USE_CACHED_DATASET="--use-cached-dataset"
+
+LOAD_PATH=$CHECKPOINT_PATH/gbs${GLOBAL_BATCH_SIZE}_ca_base
+SAVE_PATH=$CHECKPOINT_PATH/gbs${GLOBAL_BATCH_SIZE}_ca_v1
+
+if [ -n "$(ls $SAVE_PATH)" ]; then
+    RESET_ITERATION=""
+else
+    # set this parameter only when there is no saved parameter
+    RESET_ITERATION="--reset-iteration"
+fi
 
 export OMP_NUM_THREADS=48
 export LD_PRELOAD=$1
 export MYGEMM=99
 
 SEQUENCE_LENGTH=2048
-train_samples=$((300 * 1000000000 * 2 / ${SEQUENCE_LENGTH}))
+train_samples=$(echo "${train_token_in_billion} * 1000 * 1000 * 1000 / ${SEQUENCE_LENGTH}" | bc)
 
 numactl -m 4-7 -N 4-7 \
   python pretrain_gpt.py \
@@ -190,8 +200,8 @@ numactl -m 4-7 -N 4-7 \
   --train-samples $train_samples \
   --lr-decay-tokens $lr_decay_tokens \
   --lr-warmup-tokens $lr_warmup_tokens \
-  --save $CHECKPOINT_PATH/gbs${GLOBAL_BATCH_SIZE}_v21 \
-  --load $CHECKPOINT_PATH/gbs${GLOBAL_BATCH_SIZE}_v21 \
+  --save $SAVE_PATH \
+  --load $LOAD_PATH \
   --data-path $TRAIN_DATA_PATH \
   --tokenizer-type JapaneseSentencePiece \
   --vocab-file $TOKENIZER_PATH \
@@ -199,7 +209,7 @@ numactl -m 4-7 -N 4-7 \
   --split 949,51,0 \
   --distributed-backend mpi \
   --init-method-std 0.008 \
-  --lr 1.0e-4 \
+  --lr 1.0e-6 \
   --override-lr-scheduler \
   --no-load-lr-state \
   --min-lr 1.0e-6 \
@@ -227,6 +237,7 @@ numactl -m 4-7 -N 4-7 \
   --log-optimizer-states-to-tensorboard \
   --use-flush-denormal \
   $USE_CACHED_DATASET \
+  $RESET_ITERATION \
   --wandb-entity "gpt-fugaku" \
   --wandb-project "13B-2023-12-12" \
   --wandb-name "13b_dp${DATA_PARALLEL_SIZE}_pp${PIPELINE_MODEL_PARALLEL_SIZE}_tp${TENSOR_MODEL_PARALLEL_SIZE}_gbs${GLOBAL_BATCH_SIZE}"
